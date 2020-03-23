1db04ca3c2c5b84426dc638823fb528296266484
nabeek <nabeek@gmail.com>
2020-03-23 16:48:32 -0600

    Quiz fully functional

--- assets/script.js
+++ assets/script.js
@@ -22,20 +22,24 @@ var questions = [
 }]
 
 var questionArray = 0;
-var quizQuestion = 0;
+var quizQuestion = -1;
 var timeLeft = 100;
 
-function quizTimer() {
-    var quizTimer = setInterval(function(){
-        timeLeft--;
-        $("#timer").text("Time: " + timeLeft);
-        if (timeLeft <= 0) {
-            clearInterval(quizTimer);
-        }   else if (quizQuestion > 4) {
-            clearInterval(quizTimer);
+function startTimer() {
+    quizTimer = setInterval(function(){
+        $("#timer").text(timeLeft);
+        if (timeLeft !== 0) {
+            timeLeft--;
+        }   else {
+            stopTimer();
         }
     }, 1000);
 };
+
+function stopTimer() {
+    clearInterval(quizTimer);
+    $("#timer").text(timeLeft);
+}
 
 function answerCorrect() {
     $("#bannerAnswer").show();
@@ -49,15 +53,19 @@ function answerWrong() {
 };
 
 function generateButtons() {
-    for (var i = 0; i < questions[questionArray].choices.length; i++) {
-        var choicesBtn = $("<button>");
-        choicesBtn.addClass("btn bg-purple-bright font-weight-bold mb-1 quizBtn");
-        choicesBtn.attr("id", (i + 1));
-        choicesBtn.attr("type", "submit");
-        choicesBtn.attr("style", "display: block");
-        choicesBtn.text(questions[questionArray].choices[i]);
-        $(".choicesBtn").append(choicesBtn);
-    }
+    if (questionArray !== questions.length) {
+        $.each(questions[questionArray].choices, function(i, value) {
+            var choicesBtn = $("<button>");
+            choicesBtn.addClass("btn bg-purple-bright font-weight-bold mb-1 quizBtn");
+            choicesBtn.attr("id", (i + 1));
+            choicesBtn.attr("type", "submit");
+            choicesBtn.attr("style", "display: block");
+            choicesBtn.text(value);
+            $(".choicesBtn").append(choicesBtn);
+        })
+    }   else {
+            endQuiz();
+        };
 }
 
 function generateQuestion() {
@@ -68,36 +76,98 @@ function beginQuiz() {
     $("#quizBeginBtn").remove();
     $("#quizTitle").empty();
     $("#quizDirections").remove();
-    quizTimer();
+    startTimer();
     generateQuestion();
     generateButtons();
     questionArray++;
+    quizQuestion++;
 };
 
-$("#quizBeginBtn").click(beginQuiz);
-
-$(".choicesBtn").click(function(event){
-    var buttonClicked = (event.target.id);
-    $(".choicesBtn").empty();
-    generateButtons();
-    $("#quizQuestion").html(questions[questionArray].question);
-      if (buttonClicked == questions[quizQuestion].answer) {
-        answerCorrect();
-    }   else {
-        answerWrong();
+$("#quizBeginBtn").click(function() {
+    if (event.target.matches("button")){        // ensures that a button was selected, not just the div
+        beginQuiz();
     }
-    questionArray++;
-    quizQuestion++;
-    console.log(quizQuestion);
 });
 
-if (quizQuestion > 4) {
+$(".choicesBtn").click(function(event){
+    if (event.target.matches("button")){        // ensures that a button was selected, not just the div
+        var buttonClicked = (event.target.id);
+        console.log(questions[quizQuestion].answer);
+        console.log(buttonClicked);
+        $(".choicesBtn").empty();
+        generateButtons();
+        if (questionArray !== questions.length) {
+            $("#quizQuestion").html(questions[questionArray].question);
+                
+            if (buttonClicked == questions[quizQuestion].answer) {
+            answerCorrect();
+        }   else {
+            answerWrong();
+        }
+        questionArray++;
+        quizQuestion++;
+        }
+    }
+});
+
+function endQuiz() {
+    stopTimer();
+    playerScore = timeLeft;
     $("#quizTitle").text("All done!");
+    $("#quizTitle").removeClass("text-center");
+    $("#quizQuestion").empty();
+    $("#quizChoices").append("<h4>");
+    $("#quizChoices > h4").addClass("text-left").attr("id", "scoreText").html("Your final score is " + timeLeft + ".");
+    generateSubmitForm();
+}
+
+function generateSubmitForm() {
+    var submitDiv = $("<div>");
+    submitDiv.addClass("row");
+    submitDiv.attr("id", "submitScoreDiv");
+
+    var enterInitials = $("<span>");
+    enterInitials.text("Enter initials: ")
+    enterInitials.attr("style", "font-size: 1.5rem; font-weight: 500; vertical-align: middle");
+    enterInitials.addClass("col");
+    enterInitials.appendTo(submitDiv);
+    
+    var initialsInput = $("<input>");
+    initialsInput.addClass(" col-6 form-control");
+    initialsInput.attr("type", "text");
+    initialsInput.attr("id", "initialsInput");
+    initialsInput.attr("maxlength", "3");
+    initialsInput.attr("name", "initials");
+    initialsInput.appendTo(submitDiv);
+    
+    var submitScoreBtn = $("<button>");
+    submitScoreBtn.addClass("btn bg-purple-bright font-weight-bold col mx-3");
+    submitScoreBtn.attr("id", "submitScoreBtn");
+    submitScoreBtn.attr("type", "submit");
+    submitScoreBtn.text("Submit Score");
+    submitScoreBtn.appendTo(submitDiv);
+    
+    submitDiv.appendTo($("#quizChoices"));
+    
+    $("#submitScoreBtn").click(function(event) {
+        event.preventDefault();
+        var pleaseWork = "please work";
+        console.log(pleaseWork);
+        // var userInitials = initialsInput.value;
+            
+        // var highscore = {
+        //     Initials: userInitials.value,
+        //     Score: timeLeft.value
+        // };
+        
+        //localStorage.setItem("highscore", JSON.stringify(highscore));
+    });
 }
 
 
 
 
+// Highscores page-specific scripts
 $("#clearScores").click(function() {
     $("#listOfScores").empty();
 });
